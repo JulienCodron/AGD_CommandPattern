56b7334a4d49ac52b20e3402f3bd3cdd4dcf03a7
Codron Julien <codron.julien@hotmail.fr>
2025-05-26 17:46:39 +0200

    Create Commande Invoker

--- Assets/Scripts/Command/CommandInvoker.cs
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -1,20 +1,19 @@
-using Command.Main;
 using System.Collections.Generic;
 
-namespace Command.Commands
+namespace Command
 {
     public class CommandInvoker
     {
         private Stack<ICommand> commandRegistry = new Stack<ICommand>();
-
+        
         public void ProcessCommand(ICommand commandToProcess)
         {
             ExecuteCommand(commandToProcess);
             RegisterCommand(commandToProcess);
         }
-
+        
         public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
-
+        
         public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
     }
 }
